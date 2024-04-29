6c2b7936e83bfc5e19056c44b3a007f29614d9d8
Leaf Ying <leaf.ying.work@gmail.com>
2024-04-29 23:03:45 +0800

    Add a setup script and requirements.txt

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+pip install -U pip
+
+pip install -U setuptools wheel
+
+# change url according to your cuda
+mamba install -y -c "nvidia/label/cuda-11.8.0" cuda-toolkit
+
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu118
+
+pip install -r requirements.txt
