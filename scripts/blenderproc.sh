716d9b18a8a562af6e89f3f4395edd286f4fe18a
Leaf Ying <leaf.ying.work@gmail.com>
2024-06-14 06:12:04 +0800

    Remove unused blenderproch.sh

--- scripts/blenderproc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [[ -d "$HOME/blender" ]]; then
-    blenderproc --custom-blender-path "$HOME/blender" "${@:1}"
-else
-    blenderproc --blender-install-path "$HOME/blender" "${@:1}"
-fi
