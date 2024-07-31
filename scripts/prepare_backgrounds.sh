796771975e938f7ab320c968cee3719c3cb58b54
Leaf Ying <leaf.ying.work@gmail.com>
2024-07-31 23:18:56 +0800

    Save current scripts

---
+++ scripts/prepare_backgrounds.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eu
+
+python scripts/prepare_backgrounds.py ~/data/Weather/Weather_Anti_UAV/originals/Image2Weather ~/data/Weather/Weather_Anti_UAV/originals/FWID ~/data/Weather/Weather_Anti_UAV --max-samples 7000
