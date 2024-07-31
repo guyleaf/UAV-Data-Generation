fe4f9bb99c72e9c546ead5a7569d6d1750caf068
Leaf Ying <leaf.ying.work@gmail.com>
2024-08-01 04:00:54 +0800

    Save current scripts

---
+++ scripts/demo_randomization.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+
+blenderproc run -- blender/demo_randomization.py \
+~/data/UAV/blender/UAVs.blend \
+~/data/UAV/blender/assets/backgrounds/studiolights/city.exr \
+--out-dir /home/leafying/work/work_dirs/demo_randomization_adaptive_1_0 \
+--devices 0 \
+--x-range -30 30 --y-range -30 30 --samples 10 \
+--render-max-samples 1024 --render-resolution 1920 1920
--- scripts/generate_uav_samples.sh
+++ scripts/generate_uav_samples.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-blenderproc run -- blender/main.py ~/data/UAV/blender/UAVs.blend ~/data/UAV/blender/assets/backgrounds/studiolights/city.exr ~/data/Weather/Weather-Anti-UAV/backgrounds --out-dir ~/data/Weather/Weather-Anti-UAV --devices 0 1 2 3 4 5 6 7 --x-range -30 30 --y-range -30 30 --scale-range 0.01 0.4 --max-iou 0.05 --render-resolution 2560 2560 --render-tile-size 8192 --max-samples 10
+blenderproc run -- blender/main.py \
+~/data/UAV/blender/UAVs.blend \
+~/data/UAV/blender/assets/backgrounds/studiolights/city.exr \
+~/data/Weather/Weather_Anti_UAV/backgrounds \
+--out-dir ~/data/Weather/Weather_Anti_UAV \
+--devices 0 \
+--x-range -30 30 --y-range -30 30 --scale-range 0.01 0.4 --max-iof 0.2 --max-samples 6 \
+--render-max-samples 1024 --render-resolution 2560 2560 --render-tile-size 2560
