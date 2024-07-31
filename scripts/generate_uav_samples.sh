798c6cfc1c798a07902af0c7a89cb7845352db74
Leaf Ying <leaf.ying.work@gmail.com>
2024-07-31 23:30:19 +0800

    Save current scripts

--- scripts/generate_uav_samples.sh
+++ scripts/generate_uav_samples.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-blenderproc run -- blender/main.py ~/data/UAV/blender/UAVs.blend ~/data/UAV/blender/assets/backgrounds/studiolights/city.exr ~/data/Weather/Weather-Anti-UAV/backgrounds --out-dir ~/data/Weather/Weather-Anti-UAV --devices 0 1 2 3 4 5 6 7 --x-range -30 30 --y-range -30 30 --scale-range 0.01 0.3 --max-iou 0 --render-resolution 2560 2560 --render-tile-size 8192 --max-samples 10
+blenderproc run -- blender/main.py ~/data/UAV/blender/UAVs.blend ~/data/UAV/blender/assets/backgrounds/studiolights/city.exr ~/data/Weather/Weather-Anti-UAV/backgrounds --out-dir ~/data/Weather/Weather-Anti-UAV --devices 0 1 2 3 4 5 6 7 --x-range -30 30 --y-range -30 30 --scale-range 0.01 0.4 --max-iou 0.05 --render-resolution 2560 2560 --render-tile-size 8192 --max-samples 10
