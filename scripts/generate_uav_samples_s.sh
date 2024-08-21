00bd80d613055c08cc0408a359c0679951e62d4f
Leaf Ying <leaf.ying.work@gmail.com>
2024-08-21 18:18:17 +0800

    Generate Weather_Anti_UAV_S dataset with (1920, 1920) UAV resolution

--- scripts/generate_uav_samples_s.sh
+++ scripts/generate_uav_samples_s.sh
@@ -6,6 +6,6 @@ blenderproc run -- blender/main.py \
 ~/data/UAV/blender/assets/backgrounds/studiolights/city.exr \
 ~/data/Weather/Weather_Anti_UAV_S/backgrounds \
 --out-dir ~/data/Weather/Weather_Anti_UAV_S \
---devices 4 \
+--devices 0 \
 --x-range -30 30 --y-range -30 30 --scale-range 0.01 0.4 --max-iof 0.2 --max-samples 6 \
---render-max-samples 1024 --render-resolution 2560 2560 --render-tile-size 2560
+--render-max-samples 1024 --render-resolution 1920 1920 --render-tile-size 1920
