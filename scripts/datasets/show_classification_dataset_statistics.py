4b1d500f13aa6c0d6159bff82abdeeb652cc83c3
Leaf Ying <leaf.ying.work@gmail.com>
2024-09-19 17:42:53 +0800

    Support analyzing image size dist.

--- scripts/datasets/show_classification_dataset_statistics.py
+++ scripts/datasets/show_classification_dataset_statistics.py
@@ -6,6 +6,9 @@ from typing import Optional
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL.Image as Image
+from matplotlib.axes import Axes
+from matplotlib.ticker import PercentFormatter, ScalarFormatter
+from rich import print
 from utils import collect_images
 
 """expected structure of root folder
@@ -48,14 +51,11 @@ def parse_args():
     return args
 
 
-def show_classification_statistics(
+def analyze_images(
     root_dir: str,
-    title: Optional[str] = None,
     all_in_one: bool = False,
     excluded_subsets: list[str] = [],
 ):
-    root_dir = root_dir.removesuffix(os.sep)
-
     if all_in_one:
         subsets = [""]
     else:
@@ -70,9 +70,8 @@ def show_classification_statistics(
         labels.update(os.listdir(subset_dir))
     labels = sorted(labels)
 
-    max_count = 0
-    max_image_size = (0, 0)
-    subset_counts: dict[str, dict[str, int]] = defaultdict(dict)
+    areas = []
+    label_counts: dict[str, dict[str, int]] = defaultdict(dict)
     for subset in subsets:
         subset_dir = os.path.join(root_dir, subset)
 
@@ -83,44 +82,99 @@ def show_classification_statistics(
 
                 # find max image size
                 for image in images:
-                    max_area = max_image_size[0] * max_image_size[1]
                     with Image.open(image) as im:
                         area = im.size[0] * im.size[1]
-                        if area > max_area:
-                            print(image)
-                            max_image_size = im.size
+                        areas.append(area)
 
                 count = len(images)
             else:
                 count = 0
 
-            subset_counts[subset][label] = count
+            label_counts[subset][label] = count
 
-        count = sum(subset_counts[subset].values())
-        subset_counts[subset]["all"] = count
-        max_count = max(max_count, count)
+        count = sum(label_counts[subset].values())
+        label_counts[subset]["all"] = count
 
-    if title is None:
-        title = root_dir.split(os.sep)[-1]
-    x = np.arange(len(labels))
+    return labels, label_counts, areas
+
+
+def make_label_dist_plot(
+    axes: Axes,
+    labels: list[str],
+    label_counts: dict[str, dict[str, int]],
+    all_in_one: bool = False,
+):
     width = 0.2  # the width of the bars
 
-    fig, axe = plt.subplots()
-    for multiplier, (subset, counts) in enumerate(subset_counts.items()):
+    for multiplier, (subset, counts) in enumerate(label_counts.items()):
         offset = width * multiplier
-        rects = axe.bar(x + offset, list(counts.values()), width, label=subset)
-        axe.bar_label(rects, padding=3)
+        rects = axes.bar(
+            np.arange(len(counts)) + offset, list(counts.values()), width, label=subset
+        )
+        axes.bar_label(rects, padding=3)
 
-    axe.set_title(title)
-    axe.set_ylabel("Counts")
-    axe.xaxis.set_ticks(x + (width * len(subsets)) / 2, labels)
+    axes.set_title("Label")
+    axes.set_ylabel("Counts")
+    # axes.xaxis.set_ticks(x + (width * len(subsets)) / 2, labels)
+    axes.xaxis.set_ticks(axes.xaxis.get_ticklocs()[1:-1], labels)
     if not all_in_one:
-        axe.legend(loc="upper left", ncols=3)
-    axe.set_ylim(0, max_count + 5000)
-    fig.savefig(f"{title}.png")
+        axes.legend(loc="upper left", ncols=3)
 
-    print("Maximum image size:", max_image_size)
-    print("Maximum image area:", max_image_size[0] * max_image_size[1])
+    max_count = max(max(counts.values()) for counts in label_counts.values())
+    axes.set_ylim(0, max_count + 5000)
+
+
+def make_area_plot(axes: Axes, areas: list[float]):
+    n_bins = 100
+    N, bins, patches = axes.hist(areas, n_bins, color="orange")
+    mean_bins = bins[:-1] + bins[1:]
+    mean_bins /= 2
+
+    mode_i = N.argmax()
+    mode_bin = mean_bins[mode_i]
+    patches[mode_i].set_facecolor("red")
+
+    print("Min, Max image area:", min(areas), max(areas))
+    print("Average image area:", sum(areas) / len(areas))
+    print(f"Mode image area: {bins[mode_i]} ~ {bins[mode_i + 1]}")
+
+    xticks = axes.get_xticks()[1:-1]
+    xticklabels = axes.get_xticklabels()[1:-1]
+    axes.set_xticks([*xticks, mode_bin], [*xticklabels, mode_bin])
+    axes.set_xlabel("Area (pixels)")
+    axes.set_ylabel("Number of images")
+    axes.set_title("Image Area")
+
+    x_axis_formatter = ScalarFormatter()
+    x_axis_formatter.set_scientific(True)
+    axes.xaxis.set_major_formatter(x_axis_formatter)
+    axes.yaxis.set_major_formatter(PercentFormatter(xmax=len(areas)))
+
+
+def show_classification_statistics(
+    root_dir: str,
+    title: Optional[str] = None,
+    all_in_one: bool = False,
+    excluded_subsets: list[str] = [],
+):
+    labels, label_counts, areas = analyze_images(
+        root_dir, all_in_one=all_in_one, excluded_subsets=excluded_subsets
+    )
+
+    if title is None:
+        title = root_dir.removesuffix(os.sep).split(os.sep)[-1]
+
+    axes_1: Axes
+    axes_2: Axes
+    fig, (axes_1, axes_2) = plt.subplots(ncols=2, figsize=(15, 5))
+    fig.suptitle(title)
+
+    make_label_dist_plot(axes_1, labels, label_counts, all_in_one=all_in_one)
+    make_area_plot(axes_2, areas)
+
+    fig.tight_layout()
+    fig.savefig(f"{title}.png")
+    plt.close(fig)
 
 
 if __name__ == "__main__":
