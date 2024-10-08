284762b045707b48004584566814600e19b361cb
Leaf Ying <leaf.ying.work@gmail.com>
2024-10-08 19:49:06 +0800

    Update setup_gen_eval.sh

--- scripts/setup_gen_eval.sh
+++ scripts/setup_gen_eval.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-conda env create -f "$(dirname "$0")/gen_eval.yml"
+# conda env create -f "$(dirname "$0")/gen_eval.yml"
+conda create --name gen_eval python=3.10 pip
+eval "$(conda shell.bash hook)"
+conda activate gen_eval
+pip install -e git+https://github.com/layer6ai-labs/dgm-eval.git#egg=dgm_eval --src thirdparty --extra-index-url https://download.pytorch.org/whl/cu118
+pip install torch-fidelity
