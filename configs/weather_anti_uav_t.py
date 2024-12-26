84e9f06c10bac521c203bb55c6c445e5f0254ee3
Leaf Ying <leaf.ying.work@gmail.com>
2024-12-26 21:06:53 +0800

    Update demo config

--- configs/weather_anti_uav_t.py
+++ configs/weather_anti_uav_t.py
@@ -10,24 +10,20 @@ from uav_data_generation.blender.config import AREA_RANGES, BaseConfig
 class Config(BaseConfig):
     @property
     def scene_path(self) -> str:
-        return os.path.expanduser("~/data/UAV/blender/UAVs.blend")
+        return os.path.expanduser(
+            "<the folder contains blender scene and assets>/UAVs.blend"
+        )
 
     @property
     def background_path(self) -> str:
         return os.path.expanduser(
-            "~/data/UAV/blender/assets/backgrounds/studiolights/city.exr"
-        )
-
-    @property
-    def images_path(self) -> str:
-        return os.path.expanduser(
-            "~/data/UAV/Weather_Anti_UAV_T_NO_RETRY_AREA/backgrounds"
+            "<the folder contains blender scene and assets>/assets/backgrounds/studiolights/city.exr"
         )
 
     x_range: tuple[int, int] = (-30, 30)
     y_range: tuple[int, int] = (-30, 30)
     max_iof: float = 0.2
-    sample_range: tuple[int, int] = (1, 6)
+    sample_range: tuple[int, int] = (1, 4)
     area_ranges: AREA_RANGES = (
         (20**2, 32**2),
         (32**2, 96**2),
@@ -37,7 +33,7 @@ class Config(BaseConfig):
     # faster prototyping
     render_max_samples: int = 128
 
-    out_dir: str = os.path.expanduser("~/data/UAV/Weather_Anti_UAV_T_NO_RETRY_AREA_0")
+    out_dir: str = os.path.expanduser("<the folder contains UAV datasets>/Weather_Anti_UAV_T")
 
 
 if __name__ == "__main__":
