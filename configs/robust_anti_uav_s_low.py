935ed234fe1bfbace7cdcf5a68f2ee94fa93df4e
Leaf Ying <leaf.ying.work@gmail.com>
2025-05-19 22:41:09 +0800

    Add a config and generation script for Robust Anti-UAV S (Low poly ver.)

--- configs/robust_anti_uav_s_low.py
+++ configs/robust_anti_uav_s_low.py
@@ -29,7 +29,12 @@ class Config(BaseConfig):
     render_max_samples: int = 512
     motion_blur: bool = False
 
-    out_dir: str = os.path.expanduser("~/data/UAV/Robust_Anti_UAV_S")
+    # control levels of all subdivision modifiers
+    # our models are using level 1.
+    # so, 0 means low poly.
+    simplify_subdivision_render: int = 0
+
+    out_dir: str = os.path.expanduser("~/data/UAV/Robust_Anti_UAV_S_Low")
 
 
 if __name__ == "__main__":
--- scripts/blender/generate_uav_samples_s_fake.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-blenderproc run -- "$(dirname "$0")/generate_foregrounds.py" \
-configs/weather_anti_uav_s_fake.py \
---devices 1
