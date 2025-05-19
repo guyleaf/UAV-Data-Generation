82fbacdee2b073479d950a7a8892888928deb9f2
Leaf Ying <leaf.ying.work@gmail.com>
2025-05-19 22:43:34 +0800

    Rename prepare_robust_anti_uav_s.sh to prepare_robust_anti_uav_s_low.sh

--- scripts/prepare_robust_anti_uav_s_low.sh
+++ scripts/prepare_robust_anti_uav_s_low.sh
@@ -4,5 +4,5 @@ set -eu
 root="$HOME/data/Weather"
 bash "$(dirname "$0")/datasets/prepare_image2weather.sh" "$root"
 
-out="$HOME/data/UAV/Robust_Anti_UAV_S"
+out="$HOME/data/UAV/Robust_Anti_UAV_S_Low"
 python "$(dirname "$0")/pre_processings/prepare_backgrounds.py" "$root/Image2Weather/dataset" "$out/backgrounds" --dataset-names "Image2Weather" --max-samples 7500
