4084fd759cc60072b0e891f8624b8ff7f5a09546
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-16 14:48:25 +0800

    Remove the comments in preparation and generation scripts

--- scripts/blender/generate_uav_samples_low.sh
+++ scripts/blender/generate_uav_samples_low.sh
@@ -3,4 +3,4 @@ set -eu
 
 blenderproc run -- "$(dirname "$0")/generate_foregrounds.py" \
 configs/robust_anti_uav_low.py \
---devices 0 --resume
+--devices 0
--- scripts/prepare_robust_anti_uav_low.sh
+++ scripts/prepare_robust_anti_uav_low.sh
@@ -2,7 +2,7 @@
 set -eu
 
 root="$HOME/data/Weather"
-# bash "$(dirname "$0")/datasets/prepare_image2weather.sh" "$root"
+bash "$(dirname "$0")/datasets/prepare_image2weather.sh" "$root"
 
 out="$HOME/data/UAV/Robust_Anti_UAV_Low"
-python "$(dirname "$0")/pre_processings/prepare_backgrounds.py" "$root/Image2Weather/dataset" "$out/backgrounds" --dataset-names "Image2Weather" --max-samples 60000
+python "$(dirname "$0")/pre_processings/prepare_backgrounds.py" "$root/Image2Weather/dataset" "$out/backgrounds" --dataset-names "Image2Weather" --max-samples 30000
