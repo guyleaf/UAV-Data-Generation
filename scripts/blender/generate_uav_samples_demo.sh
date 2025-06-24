b98e0022b8999af329764b01c8497a9247713a2e
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-24 13:17:54 +0800

    Save current generation scripts

--- scripts/blender/generate_uav_samples_demo.sh
+++ scripts/blender/generate_uav_samples_demo.sh
@@ -4,4 +4,4 @@ set -eu
 mpiexec -n 2 -x CONSOLE_WIDTH=200 -mca orte_base_help_aggregate 0 -report-bindings \
 blenderproc run -- \
 "$(dirname "$0")/generate_foregrounds.py" \
-configs/robust_anti_uav_low.py --distributed
+configs/robust_anti_uav_demo.py --distributed
